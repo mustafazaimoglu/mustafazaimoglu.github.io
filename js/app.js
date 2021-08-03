c177f47bfb29d8f8fc3fc3c56b7eaba35206b01d
Mustafa Zaimoglu <mkuz138@gmail.com>
2021-08-03 18:45:40 +0300

    fromatted

--- js/app.js
+++ js/app.js
@@ -8,22 +8,23 @@ let projectsArr = [
         header: "MR Yazılım",
         description:
             "Arkadaşımla birlikte Türkçe içerikli yazılım öğretimine destek amacıyla oluşturulan içerinde Ne Nedir? gibi bilgilendirme amaçlı bölümlerde olan Websitesi.",
-        link:"https://www.mryazilim.net/"    
+        link: "https://www.mryazilim.net/",
     },
     {
         photoUrl: "citywalkz.jpg",
         header: "City Walkz",
-        description:"Youtube Iframe API kullanılarak oturduğumuz yerden dünya üzerindeki şehirleri yürüyerek gezme hazzı yaşatması için yapılan Websitesi.",
-        link:"https://citywalkz.herokuapp.com/"    
+        description:
+            "Youtube Iframe API kullanılarak oturduğumuz yerden dünya üzerindeki şehirleri yürüyerek gezme hazzı yaşatması için yapılan Websitesi.",
+        link: "https://citywalkz.herokuapp.com/",
     },
 ];
 
-projectsArr.forEach(p => {
+projectsArr.forEach((p) => {
     const li = document.createElement("li");
 
     const liPhoto = document.createElement("div");
     liPhoto.id = "liPhoto";
-    
+
     const liPhotoImg = document.createElement("img");
     liPhotoImg.src = "./images/projects/" + p.photoUrl;
     liPhotoImg.alt = p.photoUrl;
@@ -32,29 +33,29 @@ projectsArr.forEach(p => {
 
     const liHeader = document.createElement("div");
     liHeader.id = "liHeader";
-    liHeader.innerHTML = "<h2>"+ p.header +"</h2>"
-    
+    liHeader.innerHTML = "<h2>" + p.header + "</h2>";
+
     const liData = document.createElement("div");
     liData.id = "liData";
     liData.innerHTML = p.description;
-    
+
     const link = document.createElement("div");
     link.id = "Link";
-    
+
     const linkAElement = document.createElement("a");
     linkAElement.href = p.link;
     linkAElement.target = "_blank";
-    
+
     const span = document.createElement("span");
     span.innerHTML = " " + p.header + " ";
-    
+
     const svg = document.createElement("img");
     svg.src = "./images/svg/external-link.svg";
     svg.alt = "linkSVG";
     svg.style.height = "18px";
     svg.style.marginLeft = "2px";
     svg.style.marginBottom = "-2px";
-    
+
     liPhoto.appendChild(liPhotoImg);
 
     linkAElement.appendChild(span);
